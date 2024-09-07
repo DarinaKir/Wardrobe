15b031142e891ad5a11291171932529c0c7a7e17
TehilaDavid <tehila55555@gmail.com>
2024-09-07 23:48:11 +0300

    fix

--- app/(tabs)/wardrobe.jsx
+++ app/(tabs)/wardrobe.jsx
@@ -12,7 +12,7 @@ function Wardrobe() {
         const fetchData = async () => {
             try {
                 const response = await axios.get("http://" + serverIp + ":" + port + "/get-outfit-items");
-                setOutfitItems(response.data); // מניחים שהרשימה תגיע במבנה שציינת
+                setOutfitItems(response.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
