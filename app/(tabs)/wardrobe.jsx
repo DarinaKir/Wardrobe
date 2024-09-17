6ef9d8d2d897805a83b81d0e4abe3d1f9910996b
DarinaKir <dasha23kir@gmail.com>
2024-09-17 16:12:47 +0300

    17\09\2024

--- app/(tabs)/wardrobe.jsx
+++ app/(tabs)/wardrobe.jsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image, Modal} from 'react-native';
 import axios from "axios";
 import {serverConstants} from '../../constants/serverConstants'
 import {SafeAreaView} from "react-native-safe-area-context";
 import * as ImagePicker from 'expo-image-picker';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Wardrobe() {
     const [outfitItems, setOutfitItems] = useState([]);
     const [imageUri, setImageUri] = useState(null); // משתנה לשמירת URI של התמונה שנבחרה
+    const [modalVisible, setModalVisible] = useState(false); // משתנה לניהול מצב התצוגה של ה-Modal
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,6 +39,7 @@ function Wardrobe() {
 
         if (!result.canceled) {
             setImageUri(result.assets[0].uri); // שמירת ה-URI של התמונה
+            setModalVisible(false); // סגירת ה-Modal
         }
     };
 
@@ -49,7 +53,13 @@ function Wardrobe() {
 
         if (!result.canceled) {
             setImageUri(result.assets[0].uri);
+            setModalVisible(false);
         }
+    };
+
+    // פונקציה למחיקת התמונה
+    const deleteImage = () => {
+        setImageUri(null);
     };
 
     const renderItem = ({ item }) => (
@@ -67,22 +77,59 @@ function Wardrobe() {
         <SafeAreaView style={styles.container}>
             <Text style={styles.header}>Wardrobe</Text>
 
-            {/* כפתור מותאם אישית */}
-            <TouchableOpacity onPress={pickImage} style={styles.button}>
-                <Text style={styles.buttonText}>Pick an image from gallery</Text>
-            </TouchableOpacity>
+            {/*/!* כפתור מותאם אישית *!/*/}
+            {/*<TouchableOpacity onPress={pickImage} style={styles.button}>*/}
+            {/*    <Text style={styles.buttonText}>Pick an image from gallery</Text>*/}
+            {/*</TouchableOpacity>*/}
 
-            {/* כפתור לצילום תמונה */}
-            <TouchableOpacity onPress={takePhoto} style={styles.button}>
-                <Text style={styles.buttonText}>Take a photo</Text>
-            </TouchableOpacity>
+            {/*/!* כפתור לצילום תמונה *!/*/}
+            {/*<TouchableOpacity onPress={takePhoto} style={styles.button}>*/}
+            {/*    <Text style={styles.buttonText}>Take a photo</Text>*/}
+            {/*</TouchableOpacity>*/}
+
+            {/* כפתור לפתיחת ה-Modal */}
+            <View style={styles.centeredButtonContainer}>
+                <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.iconButton}>
+                    <MaterialIcons name="add-photo-alternate" size={30} color="white" />
+                </TouchableOpacity>
+            </View>
+
+            {/* Modal עם שני הכפתורים */}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={pickImage} style={styles.iconButtonSquare}>
+                                <MaterialIcons name="photo-library" size={30} color="white" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={takePhoto} style={styles.iconButtonSquare}>
+                                <MaterialIcons name="camera-alt" size={30} color="white" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.iconButtonSquare}>
+                                <MaterialIcons name="close" size={30} color="white" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
 
             {imageUri && (
-                <Image
-                    source={{ uri: imageUri }}
-                    style={styles.image}
-                    resizeMode="contain" // שימוש ב-resizeMode כדי להציג את כל התמונה
-                />
+                <View style={styles.centeredButtonContainer}>
+                    <Image
+                        source={{ uri: imageUri }}
+                        style={styles.image}
+                        resizeMode="contain" // שימוש ב-resizeMode כדי להציג את כל התמונה
+                    />
+                    <TouchableOpacity onPress={deleteImage} style={styles.deleteButton}>
+                        <MaterialIcons name="delete" size={30} color="white" />
+                    </TouchableOpacity>
+                </View>
             )}
 
             <View style={styles.table}>
@@ -123,6 +170,8 @@ const styles = StyleSheet.create({
         padding: 10,
         borderBottomWidth: 1,
         borderColor: '#ddd',
+
+        justifyContent: 'center', // הכפתורים יהיו ממורכזים בשורה
     },
     cell: {
         flex: 1,
@@ -133,30 +182,58 @@ const styles = StyleSheet.create({
         padding: 5,
         fontWeight: 'bold',
     },
-    pickerContainer: {
-        marginTop: 20,
-    },
-    picker: {
-        height: 50,
-        width: '100%',
-    },
     image: {
         width: '100%',  // התמונה תתפרס על כל הרוחב הזמין
         height: 300,    // גובה קבוע של 300 פיקסלים (אפשר לשנות בהתאם לצרכים שלך)
         marginTop: 20,
         marginBottom: 20,
     },
-    button: {
-        backgroundColor: '#007BFF',
-        padding: 10,
-        borderRadius: 5,
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalContent: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'center',
+        width: '80%',
+    },
+    deleteButton: {
+        backgroundColor: '#FF0000',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 60,
+        height: 60,
         marginBottom: 20,
     },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: 'bold',
+    iconButton: {
+        backgroundColor: '#007BFF',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 60,
+        height: 60,
+        marginBottom: 10,
+    },
+    iconButtonSquare: {
+        backgroundColor: '#007BFF',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 60,
+        height: 60,
+        marginHorizontal: 10, // מרווח בין הכפתורים בשורה
+    },
+    centeredButtonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
 
